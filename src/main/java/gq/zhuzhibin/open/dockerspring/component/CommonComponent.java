436128f3987716f1794ec9fe64f34d27608d4793
zhuzhibin <zhuzzb@qq.com>
2022-01-09 23:46:26 +0800

    包路径整理

---
+++ src/main/java/gq/zhuzhibin/open/dockerspring/component/CommonComponent.java
@@ -0,0 +1,11 @@
+package gq.zhuzhibin.open.dockerspring.component;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class CommonComponent {
+
+    public String test() {
+        return "666";
+    }
+}
--- src/main/java/gq/zhuzhibin/open/dockerspring/controller/MainController.java
+++ src/main/java/gq/zhuzhibin/open/dockerspring/controller/MainController.java
@@ -1,18 +1,22 @@
 package gq.zhuzhibin.open.dockerspring.controller;
 
-import org.springframework.http.HttpRequest;
-import org.springframework.stereotype.Controller;
+import gq.zhuzhibin.open.dockerspring.component.CommonComponent;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import javax.annotation.Resource;
 
 @RestController
 @RequestMapping("api")
 @CrossOrigin(origins = "*", maxAge = 1800L)
 public class MainController {
 
+    @Resource
+    private CommonComponent commonComponent;
+
     @RequestMapping("test")
     public String index(){
-        return "zzb";
+        return commonComponent.test();
     }
 }
